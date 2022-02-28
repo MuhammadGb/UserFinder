5fde13daa129b89dceddcf873c0ca41eb5590770
Muhammad <muhammad17gb@gmail.com>
2022-02-28 11:55:51 +0100

    user search hover

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const UserItem = ({ currentUsers }) => {
   return (
     <div className="user-grid">
       {currentUsers &&
-        currentUsers.map((user) => (
+        currentUsers.map((user, index) => (
           <div className="card" key={user.id}>
-            <div className="text-center">
-              <img
-                src={user.avatar_url}
-                alt=""
-                className="round-img"
-                style={imageStyle}
-              />
+            <img
+              src={user.avatar_url}
+              alt=""
+              className="round-img"
+              style={imageStyle}
+              onMouseDown={() => {
+                document.querySelectorAll(".lower-text")[index].style.display =
+                  "block";
+              }}
+              onMouseEnter={() => {
+                document.querySelectorAll(".lower-text")[index].style.display =
+                  "block";
+              }}
+              onMouseLeave={() => {
+                document.querySelectorAll(".lower-text")[index].style.display =
+                  "none";
+              }}
+            />
+            <div className="lower-text">
               <h3 className="card-text">User: {user.login}</h3>
               <h4 className="card-text">Score: {user.score}</h4>
               <h3 className="card-text">
